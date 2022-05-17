ba1e4d92ca04b6d680617d44c67095d558479773
DanielLemosELEMESMO <daniel.dcl.es@gmail.com>
2022-05-16 22:27:41 -0300

    1 binaries uploaded

--- src/Components/Blocos/Introducao.js
+++ src/Components/Blocos/Introducao.js
@@ -5,7 +5,8 @@ const Introducao = (props) => {
   return (
     <div>
       <Bloco>
-        <h1>Olá {props.name}</h1>
+        <h1>Olá</h1>
+        <img src="../../assets/images/logo grande.png" alt="Logo"/>
       </Bloco>
     </div>
   );
