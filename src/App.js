37e26b8c7d60d26c11947f4839233ca9f46d59fd
JoaoPedroAC <joaopedro.alm.c@gmail.com>
2022-05-18 22:45:27 -0300

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <div className="main">
       <Cabecalho />
-      <Torres />
       <Introducao />
+      <Torres />
     </div>
   );
 }
--- src/Components/Torres.js
+++ src/Components/Torres.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './torres.css';
 const torres = (props) => {
-  return <div className='area-torres'>
-    <img src="" alt="">
-  </div>;
+  return (
+    <div className="area-torres">
+      <div className='flex-torres'>
+        <img
+        className='torres-altas'
+          src="https://stackblitz.com/files/react-behpjs/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/imagens/3%201.png"
+          alt="torre alta"
+        />
+        {/* torres-altas */}
+        
+        <div className='torre-oeste'>
+        <h3>TORRE OESTE</h3>
+        <p>Com 158 metros e 50 pavimentos, a torre oeste é a maior do espírito santo.   Possui subsolo, térreo, garagem, pilotis e 47 andares com apartamentos de alto padrão. Nesta torre os apartamentos possuem 3 suítes e um acabamento de luxo.   A maior planta possui 176,41m².   Serão 3 vagas de estacionamento por unidade, podendo também adquirir uma quarta vaga.</p>
+        </div>
+        {/* torre-oeste */}
+      </div>
+      {/* flex-torres */}
+    </div>
+  );
 };
 export default torres;
