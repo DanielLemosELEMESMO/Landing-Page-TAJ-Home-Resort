1338d4bca1999ed031c867663cd23bbbf997a372
JoaoPedroAC <joaopedro.alm.c@gmail.com>
2022-05-18 23:05:42 -0300

    fazendo torres

--- src/Components/Torres.js
+++ src/Components/Torres.js
@@ -4,32 +4,39 @@ const torres = (props) => {
   return (
     <div className="area-torres">
       <div className="flex-torres">
-        <img
-          className="torres-altas"
-          src="https://stackblitz.com/files/react-behpjs/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/imagens/3%201.png"
-          alt="torre alta"
-        />
-        {/* torres-altas */}
+        <div className="T-A">
+          <img
+            className="torres-altas"
+            src="https://stackblitz.com/files/react-behpjs/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/imagens/3%201.png"
+            alt="torre alta"
+          />
+          {/* torres-altas */}
 
-        <div className="torre-oeste">
-          <h3>TORRE OESTE</h3>
-          <p>
-            Com 158 metros e 50 pavimentos, a torre oeste é a maior do espírito
-            santo.   Possui subsolo, térreo, garagem, pilotis e 47 andares com
-            apartamentos de alto padrão. Nesta torre os apartamentos possuem 3
-            suítes e um acabamento de luxo.   A maior planta possui 176,41m².
-              Serão 3 vagas de estacionamento por unidade, podendo também
-            adquirir uma quarta vaga.
-          </p>
+          <div className="torre-oeste">
+            <h3>TORRE OESTE</h3>
+            <p>
+              Com 158 metros e 50 pavimentos, a torre oeste é a maior do
+              espírito santo.   Possui subsolo, térreo, garagem, pilotis e 47
+              andares com apartamentos de alto padrão. Nesta torre os
+              apartamentos possuem 3 suítes e um acabamento de luxo.   A maior
+              planta possui 176,41m².   Serão 3 vagas de estacionamento por
+              unidade, podendo também adquirir uma quarta vaga.
+            </p>
+          </div>
+          {/* torre-oeste */}
         </div>
-        {/* torre-oeste */}
+        {/* T-A */}
 
-        <img
-        className='torres-baixas'
-          src="https://stackblitz.com/files/react-behpjs/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/Imagens/Torres%201.png"
-          alt="torre baixa"
-        />
-        <div></div>
+        <div>
+          <img
+            className="torres-baixas"
+            src="https://stackblitz.com/files/react-behpjs/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/Imagens/Torres%201.png"
+            alt="torre baixa"
+          />
+          <div className='torre-leste'>
+
+          </div>
+        </div>
       </div>
       {/* flex-torres */}
     </div>
