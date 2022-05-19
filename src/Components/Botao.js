59aa7f75c6b3a91f25936f633c1bdd7a0e1bf7fa
DanielLemosELEMESMO <daniel.dcl.es@gmail.com>
2022-05-18 22:55:20 -0300

    Comecando a desenvolver a Introducao, e adicionado o componente botao

---
+++ src/Components/Botao.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Botao = (props) => {
+  return <button className={props.className}>{props.title}</button>;
+};
+
+export default Botao;
--- src/Components/Introducao.js
+++ src/Components/Introducao.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Botao from './Botao';
 
 const Introducao = (props) => {
   return (
-    <div>
-      <h1>Olá</h1>
-      <img
-        src="https://stackblitz.com/files/react-rgfmwx/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/imagens/desktop_logo_grande.png"
-        alt="Logo"
+    <div class="bloco">
+      <div>
+        <img
+          src="https://stackblitz.com/files/react-rgfmwx/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/imagens/desktop_logo_grande.png"
+          alt=""
+        />
+      </div>
+      <Botao
+        title="MARQUE UMA VISITAÇÃO"
+        className="button_outlined_yellow glow"
       />
     </div>
   );
