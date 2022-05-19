b9821f6685f18be9a4e769059a7d8cd7d9f8ea5e
JoaoPedroAC <joaopedro.alm.c@gmail.com>
2022-05-18 22:03:31 -0300

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Cabecalho from './Components/Cabecalho.js';
 import Introducao from './Components/Introducao.js';
+import Torres from './Components/Torres.js';
 import './style.css';
 
 export default function App() {
   return (
     <div className="main">
       <Cabecalho />
-      {/* <Introducao /> */}
+      <Torres />
+      <Introducao />
     </div>
   );
 }
