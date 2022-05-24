36c569cacf663cd89163906b3ce641fdd5133d92
DanielLemosELEMESMO <64296181+DanielLemosELEMESMO@users.noreply.github.com>
2022-05-24 08:33:32 -0300

    Evolucao da introducao e criacao do componente "video introdutorio"

--- src/Components/Blocos/Introducao/Introducao.js
+++ src/Components/Blocos/Introducao/Introducao.js
@@ -4,17 +4,31 @@ import "./Introducao.styles.css";
 
 const Introducao = (props) => {
   return (
-    <div className="bloco introducao">
+    <div className="bloco bloco-introducao">
       <div styles="width:100px !important">
         <img
           src="https://stackblitz.com/files/react-rgfmwx/github/DanielLemosELEMESMO/Landing-Page-TAJ-Home-Resort/master/src/Components/imagens/desktop_logo_grande.png"
           alt=""
         />
       </div>
+
+      <div className="texto-principal">
+        <p>
+          <span>O luxuojo Taj Home Resort</span> que já está em construção{" "}
+          <span>desafia o comum, te oferece o que é tão precioso na vida:</span>{" "}
+          mais tempo para aproveitar o que <span>vale a pena.</span> Aqui,{" "}
+          <span>cada segundo gasto é um segundo ganho.</span>
+        </p>
+      </div>
+
       <Botao
         title="MARQUE UMA VISITAÇÃO"
         className="button_outlined_yellow glow"
       />
+
+      <div>
+        <h3>VENHA CONHECER O PARAÍSO DAS ARÁBIAS, LOCALIZADO EM VILA VELHA</h3>
+      </div>
     </div>
   );
 };
---
+++ src/Components/Blocos/VideoIntrodutorio/videoIntrodutorio.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const VideoIntrodutorio = (props) => {
+  return (
+    <div>
+      <div>texto 1</div>
+
+      <div>texto 2</div>
+
+      <div>video</div>
+
+      <div>botao</div>
+    </div>
+  );
+};
+
+export default VideoIntrodutorio;
