fdc620f8997e84d1440532f8f01c5b164739da8d
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-05-26 22:52:32 -0300

    criei o formulario e ajustei o tamanho de alguns outros css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cabecalho from "./Components/Blocos/Cabecalho/Cabecalho";
 import Introducao from "./Components/Blocos/Introducao/Introducao";
 import Torres from "./Components/Blocos/Torres/Torres.js";
 import Bem from "./Components/Blocos/Bem-Estar/Bem.js";
+import Formulario from "./Components/Blocos/Formulario/Formulario.js";
 import "./style.css";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       {/* <Introducao /> */}
       <Torres />
       <Bem />
+      <Formulario/>
     </div>
   );
 }
---
+++ src/Components/Blocos/Formulario/Formulario.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './formulario.css';
+
+const inputPhone = document.getElementById('telefone');
+// precisa-se criar regra para o input do telefone, para caso escrevam errado alguma coisa ou coloquem string ao inves de numero
+
+function formulario(props) {
+	return (
+		<div className='area-form-flex'>
+			<form>
+				<label reactFor='nome'>
+					<p>Nome</p>
+					<input type='text' key='nome' placeholder='Nome completo' required />
+				</label>
+
+				<label reactFor='telefone'>
+					<p>Telefone</p>
+					<input
+						type='number'
+						key='telefone'
+						placeholder='Telefone com DDD'
+						required
+					/>
+				</label>
+
+				<label reactFor='cidade'>
+					<p>Cidade</p>
+					<input type='text' key='cidade' required />
+				</label>
+
+				<label reactFor='estado'>
+					<p>Estado</p>
+					<input type='text' key='estado' required />
+				</label>
+
+				<label reactFor='email'>
+					<p>E-mail</p>
+					<input
+						type='email'
+						key='email'
+						required
+						placeholder='exemplo@email.com'
+					/>
+				</label>
+
+				<label reactFor='mensagem'>
+					<p>Mensagem</p>
+					<textarea required key='mensagem'></textarea>
+				</label>
+
+				<label reactFor='termo' className='termo'>
+					<input type='checkbox' key='termo' required />
+					<p>Aceito receber o contato de um consultor</p>
+				</label>
+				{/* colocar aqui o botao */}
+			</form>
+			<content className='contato-texto'>
+				<h2>Entre em contato conosco e marque uma visita.</h2>
+				<p>
+					Não perca a oportunidade de proporcionar a você e sua família essa
+					experiência única.
+				</p>
+				<p>
+					Venha visitar o Taj Home Resort e conhecer mais sobre esse paraíso
+					localizado em Vila Velha, onde os seus sonhos podem se tornar
+					realidade.
+				</p>
+				<img src='' alt='TAJ' />
+			</content>
+		</div>
+	);
+}
+export default formulario;
